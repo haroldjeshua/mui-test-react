3c7502adfd4bafa1871f62683614aa4502ade0c1
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-05 15:31:22 +0800

    added appbar for testing syntax practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Combobox from './components/Combobox';
 import Buttons from './components/Buttons';
 import FloatingActionButtons from './components/FloatingActionButtons';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
 	return (
 		<div className="container">
+			<ResponsiveAppBar />
 			<Buttons />
 			<Combobox />
 			<FloatingActionButtons />
